13b075c631943485fb7742dbd08ece33206cdce8
schwarb <schwarb@purdue.edu>
2015-01-26 13:25:22 -0500

    Cost/Toxicity minimization
    This script attempts to create an algorithm to minimize the above
    values.

---
+++ Modeling/Cost_ToxicityMin.m
@@ -0,0 +1,9 @@
+%%%This script is to create two basic algorithms, one to minimize cost of a
+%%%particular mixture of Qdots, and the other to minimize toxicity impact.
+%%%Both while maintaining a particular band energy.
+%%Calculations for individual band gap energies.
+BE1 = BandGapDot(1.92,4.5e-9,3.6) %Band energy 1
+BE2 = BandGapDot(1.32,3.5e-9,9.2) %Band energy 2
+BE3 = BandGapDot(1.5,1.5e-9,4.0)  %Band energy 3
+BE4 = BandGapDot(1.71,4.9e-9,14.0)%Band energy 4
+BE5 = BandGapDot(1.18,2.7e-9,7)   %Band energy 5
